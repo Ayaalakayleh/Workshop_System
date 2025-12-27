5a92dc00b4568679584108cc6331da8834400634
Aya Akaylah <alakayleh.aya@gmail.com>
2025-12-28 01:24:49 +0300

    Repair Order Request Report - In progress...

--- Workshop.Web/Models/RepairOrderRequestReportModel.cs
+++ Workshop.Web/Models/RepairOrderRequestReportModel.cs
@@ -1,29 +1,43 @@
-﻿namespace Workshop.Web.Models
+﻿using Workshop.Core.DTOs;
+using Workshop.Core.DTOs.Vehicle;
+
+namespace Workshop.Web.Models
 {
     public class RepairOrderRequestReportModel
     {
+        public int? WIPId { get; set; }
+        public int? MovementId { get; set; }
         public int? VehicleNo { get; set; }
         public DateTime? Date { get; set; }
         public DateTime? TimeReceived { get; set; }
-        public string? Make { get; set; }
-        public string? Model { get; set; }
-        public string? Year { get; set; }
-        public string? PlateNumber { get; set; }
-        public string? Mileage { get; set; }
-        public string? VIN { get; set; }
-        public DateTime? ContractExpDate { get; set; }
+        //public string? Make { get; set; }
+        //public string? Model { get; set; }
+        //public int? Year { get; set; }
+        //public string? PlateNumber { get; set; }
+        //public decimal? Mileage { get; set; }
+        //public string? VIN { get; set; }
+
+        public VehicleInfoModel VehicleInfo { get; set; }
+        public string? ContractExpDate { get; set; }
         public string? CompanyName { get; set; }
+        public string? FuelLevel { get; set; }
         public DateTime? InsuranceExpDate { get; set; }
         public string? CustomerName { get; set; }
+        //public string? ColorName { get; set; }
+        public string? Trim { get; set; }
         public DateTime? EstimaraExpDate { get; set; }
         public string? MobileNumber { get; set; }
         public DateTime? MVPIExpDate { get; set; }
         public string? Complaint { get; set; }
-        public DateTime? DateIn { get; set; }
-        public DateTime? TimeIn { get; set; }
-        public DateTime? DateOut { get; set; }
-        public DateTime? TimeOut { get; set; }
+        public string? DateIn { get; set; }
+        public TimeSpan? TimeIn { get; set; }
+        public string? DateOut { get; set; }
+        public TimeSpan? TimeOut { get; set; }
         public string? AccountNo { get; set; }
-        public List<string>? Services { get; set; }
+        //public List<string>? Services { get; set; }
+        public List<VehicleChecklist>? VehicleCkecklist { get; set; }
+        public List<TyreChecklist>? TyreCkecklist { get; set; }
+        public List<CreateWIPServiceDTO>? Services { get; set; }
+        public List<ItemModel>? Items { get; set; }
     }
 }
---
+++ Workshop.Web/Models/VehicleInfoModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Workshop.Web.Models
+{
+    public class VehicleInfoModel
+    {
+        public string? Make { get; set; }
+        public string? Model { get; set; }
+        public int? Year { get; set; }
+        public string? PlateNumber { get; set; }
+        public decimal? Mileage { get; set; }
+        public string? VIN { get; set; }
+        public string? ColorName { get; set; }
+
+    }
+}
