b201f112ec4099f0efa62736e4e4f9b34bbc66eb
Mahmoud Hamdan <mahmoud.hamdan@acs-techs.com>
2026-01-08 08:18:07 +0300

    - Report Service Ready to use "See Example in Inventory module"

---
+++ ReportsService/Controllers/ReportsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ReportsService.Controllers
+{
+    public class ReportsController : ApiController
+    {
+        private readonly string _apiBaseUrl;
+        private readonly HttpClient _httpClient;
+
+        public ReportsController()
+        {
+            _apiBaseUrl = System.Configuration.ConfigurationManager.AppSettings["MainApiBaseUrl"];
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri(_apiBaseUrl);
+        }
+
+        [HttpGet]
+        [Route("api/reports/test")]
+        public IHttpActionResult Test()
+        {
+            return Ok(new
+            {
+                message = "ReportsService is running!",
+                timestamp = DateTime.Now,
+                baseUrl = _apiBaseUrl
+            });
+        }
+    }
+}
