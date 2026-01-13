ad0d779534e3cfa17d89840d0946b9f999378de3
Aya Akaylah <alakayleh.aya@gmail.com>
2026-01-13 15:31:03 +0300

    conflict

--- Workshop.Web/wwwroot/js/Definitions/WIPSelectionGridItem.js
+++ Workshop.Web/wwwroot/js/Definitions/WIPSelectionGridItem.js
@@ -181,24 +181,24 @@ function updateSelectionCount(count) {
 //        WarehouseId: x.WarehouseId ?? null
 //    }));
 
-    return $.ajax({
-        url: window.RazorVars.insertItemsUrl,
-        method: "POST",
-        dataType: "json",
-        data: {
-            WIPId: wipId,
-            ItemsList: baseItems
-        },
-        error: function () {
-            Swal.fire({
-                icon: "error",
-                title: theMainLang == "en" ? 'Error Happened' : "حصل خطأ ما",
-                showConfirmButton: true,
-                confirmButtonText: theMainLang == "en" ? 'Ok' : "حسناً",
-            });
-        }
-    });
-}
+    //return $.ajax({
+    //    url: window.RazorVars.insertItemsUrl,
+    //    method: "POST",
+    //    dataType: "json",
+    //    data: {
+    //        WIPId: wipId,
+    //        ItemsList: baseItems
+    //    },
+    //    error: function () {
+    //        Swal.fire({
+    //            icon: "error",
+    //            title: theMainLang == "en" ? 'Error Happened' : "حصل خطأ ما",
+    //            showConfirmButton: true,
+    //            confirmButtonText: theMainLang == "en" ? 'Ok' : "حسناً",
+    //        });
+    //    }
+    //});
+//}
 
 function showItemSelectionModal() {
     initializeItemSelectionGrid();
