fd4877ce432e04e18a7145148a0b3f9b592e88de
Aya Akaylah <alakayleh.aya@gmail.com>
2026-02-07 23:24:29 +0300

    Recall Default Active

--- Workshop.Core/DTOs/RecallDTO.cs
+++ Workshop.Core/DTOs/RecallDTO.cs
@@ -7,8 +7,8 @@
 		public string? Description { get; set; }
 		public DateTime? StartDate { get; set; }
 		public DateTime? EndDate { get; set; }
-		public bool IsActive { get; set; }
-		public List<VehicleRecallDTO>? Vehicles { get; set; }
+		public bool IsActive { get; set; } = true;
+        public List<VehicleRecallDTO>? Vehicles { get; set; }
 	}
 
 	public class RecallDTO : RecallBaseDTO
