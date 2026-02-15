fcbf1d6625405770f130504031d6eb3e6bd989e1
Aya Akaylah <alakayleh.aya@gmail.com>
2026-02-15 17:35:48 +0300

    Add Petty Cash Screens

---
+++ Workshop.Web/Controllers/PettyCashController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Workshop.Web.Models;
+
+namespace Workshop.Web.Controllers
+{
+    [SessionTimeout]
+
+    public class PettyCashController : BaseController
+    {
+
+        public PettyCashController(IConfiguration configuration, IWebHostEnvironment env, IMemoryCache cache) : base(cache, configuration, env)
+        {
+        }
+
+        [CustomAuthorize(Permissions.PettyCash.Expenses)]
+
+        public ActionResult Index()
+        {
+            ViewBag.HostName = _configuration["ApiSettings:AccountingUrl"];
+            return View();
+        }
+        [CustomAuthorize(Permissions.PettyCash.History)]
+
+        public ActionResult History()
+        {
+            ViewBag.HostName = _configuration["ApiSettings:AccountingUrl"];
+            return View();
+        }
+        [CustomAuthorize(Permissions.PettyCash.Request)]
+
+        public ActionResult PettyCashRequest()
+        {
+            ViewBag.HostName = _configuration["ApiSettings:AccountingUrl"];
+            return View();
+        }
+        [CustomAuthorize(Permissions.PettyCash.Close)]
+
+        public ActionResult PettyCashClose()
+        {
+            ViewBag.HostName = _configuration["ApiSettings:AccountingUrl"];
+            return View();
+        }
+    }
+}
